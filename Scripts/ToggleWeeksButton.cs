58abf63e4a386a9abd8dad76ea2f72a8089c8bd2
Azukimochi <Azukimochi25565@gmail.com>
2023-09-13 15:57:25 +0900

    add Button change function

---
+++ Scripts/ToggleWeeksButton.cs
@@ -0,0 +1,21 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public class ToggleWeeksButton: UdonSharpBehaviour
+{
+    [SerializeField] private int id;
+    [SerializeField] private ToggleWeeksParent pareint;
+    
+    void Start()
+    {
+        
+    }
+    public void Pressed()
+    {
+        Debug.Log("Clicked");
+        pareint.OnClicked(id);
+    }
+}
--- Scripts/ToggleWeeksParent.cs
+++ Scripts/ToggleWeeksParent.cs
@@ -1,13 +1,36 @@
 ﻿
 using UdonSharp;
 using UnityEngine;
+using UnityEngine.UI;
 using VRC.SDKBase;
 using VRC.Udon;
 
 public class ToggleWeeksParent : UdonSharpBehaviour
 {
+
+    [SerializeField] private GameObject[] weeks;
+    
+    
     void Start()
     {
         
+    }
+
+    public void OnClicked(int id)
+    {
+        Debug.Log($"Clicked id:{id}");
+        if (weeks.Length != 7)
+            return;
+        
+        Debug.Log($"SetColor");
+
+        for (int i = 0; i < weeks.Length; i++)
+        {
+            Button button = weeks[i].GetComponent<Button>();
+            if (i == id)
+                button.image.color = Color.gray;
+            else
+                button.image.color = Color.black;
+        }
     }
 }
