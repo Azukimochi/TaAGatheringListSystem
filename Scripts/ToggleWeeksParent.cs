f9ba47163cb9c540aa812dd45af6da4d84f17257
Azukimochi <azukimochi25565@gmail.com>
2023-10-11 15:00:24 +0900

    add Default week select from today's DayOfWeek

--- Scripts/ToggleWeeksParent.cs
+++ Scripts/ToggleWeeksParent.cs
@@ -1,4 +1,6 @@
-﻿using UdonSharp;
+﻿using System;
+using UdonSharp;
+using UnityEditor.Graphs;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -18,6 +20,7 @@ namespace io.github.Azukimochi
 
         public void OnClicked(Week week)
         {
+            var today = getWeekFromToday();
             if (weeks.Length != 8)
                 return;
 
@@ -28,8 +31,44 @@ namespace io.github.Azukimochi
                     button.image.color = Color.gray;
                 else
                     button.image.color = Color.black;
+
+                if (i == (int)today)
+                {
+                    button.image.color = Color.blue;
+                }
             }
+
             parent.SelectWeek(week);
+        }
+
+        public void initDefaultSelectWeekOfDay()
+        {
+            var week = getWeekFromToday();
+            OnClicked(week);
+        }
+        public Week getWeekFromToday()
+        {
+            var toDayOfWeek = DateTime.Now.DayOfWeek;
+
+            switch (toDayOfWeek)
+            {
+                case DayOfWeek.Sunday:
+                    return Week.Sunday;
+                case DayOfWeek.Monday:
+                    return Week.Monday;
+                case DayOfWeek.Tuesday:
+                    return Week.Tuesday;
+                case DayOfWeek.Wednesday:
+                    return Week.Wednesday;
+                case DayOfWeek.Thursday:
+                    return Week.Thursday;
+                case DayOfWeek.Friday:
+                    return Week.Friday;
+                case DayOfWeek.Saturday:
+                    return Week.Saturday;
+            }
+
+            return Week.None;
         }
     }
 }
